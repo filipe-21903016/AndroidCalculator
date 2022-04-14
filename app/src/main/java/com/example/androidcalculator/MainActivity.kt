4904287cf18add4009bd5aa843a3257a6ea92c08
fzarco <filipezarcocoutinho@gmail.com>
2022-04-14 13:47:06 +0100

    Fiquei no onBackPressed (nao comecei)

--- app/src/main/java/com/example/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculator/MainActivity.kt
@@ -1,23 +1,62 @@
 package com.example.androidcalculator
 
+import android.app.Notification
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidcalculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (!screenRotated(savedInstanceState)) {
+            NavigationManager.goToCalculatorFragment(supportFragmentManager)
+        }
+
+    }
+
+    private fun screenRotated(savedInstanceState: Bundle?): Boolean {
+        return savedInstanceState != null
     }
 
     override fun onStart() {
         super.onStart()
-        NavigationManager.goToCalculatorFragment(supportFragmentManager)
+        setSupportActionBar(binding.toolbar)
+        setupDrawerMenu()
     }
+
+    private fun setupDrawerMenu() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawer, binding.toolbar,
+            R.string.drawer_open, R.string.drawer_close
+        )
+        binding.navDrawer?.setNavigationItemSelectedListener {
+            onClickNavigationItem(it)
+        }
+        binding.drawer?.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun onClickNavigationItem(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_calculator -> NavigationManager.goToCalculatorFragment(supportFragmentManager)
+        }
+        binding.drawer?.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
 }
