9087860776c8af11e36bcbec35364b3c07175890
fzarco <filipezarcocoutinho@gmail.com>
2022-05-12 22:35:23 +0100

    Ficha 10 parte do mapa feita?? Até "Runtime Permissions"

---
+++ app/src/main/java/com/example/androidcalculator/FusedLocation.kt
@@ -0,0 +1,57 @@
+package com.example.androidcalculator
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Looper
+import com.google.android.gms.location.*
+
+@SuppressLint("MissingPermission")
+class FusedLocation private constructor(context: Context) : LocationCallback(){
+    private val TAG = FusedLocation::class.java.simpleName
+
+    private val TIME_BETWEEN_UPDATES = 20 * 1000L
+
+    private var client = FusedLocationProviderClient(context)
+
+    private  var locationRequest = LocationRequest.create().apply {
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        interval = TIME_BETWEEN_UPDATES
+    }
+
+    init {
+
+        //instancia objecto que permite definir as configs
+        val locationSettingsRequest = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .build()
+
+        //aplicar as configs ao servico de localizacao
+        LocationServices.getSettingsClient(context)
+            .checkLocationSettings(locationSettingsRequest)
+
+        client.requestLocationUpdates(locationRequest, this, Looper.getMainLooper())
+    }
+
+    companion object{
+        private var listener: OnLocationChangedListener? = null
+        private var instance: FusedLocation? = null
+
+        fun registerListener(listener: OnLocationChangedListener){
+            this.listener = listener
+        }
+
+        fun unregisterListener(){
+            listener = null
+        }
+
+        fun notifyListeners(locationResult: LocationResult){
+            val location = locationResult.lastLocation
+            listener?.onLocationChanged(location.latitude, location.longitude)
+        }
+
+        fun start(context:Context)
+        {
+            instance = if (instance == null) FusedLocation(context) else instance
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidcalculator/OnLocationChangedListener.kt
@@ -0,0 +1,6 @@
+package com.example.androidcalculator
+
+interface OnLocationChangedListener {
+    fun onLocationChanged(latitude: Double, longitude: Double)
+
+}
